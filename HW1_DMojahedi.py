6b6f1d45d111e47e8b22f66be9cc0b14992b517d
dmoja <37120407+dmoja@users.noreply.github.com>
2018-09-10 18:29:13 -0400

    Still working on Tree

--- HW1_DMojahedi.py
+++ HW1_DMojahedi.py
@@ -28,11 +28,11 @@ data = df.iloc[:, :].values
 
 parent_node = 'null'  # Boolean value to determine if node is root.
 node = {}  # Node array
-node_iterator = 0  # A value for iterating node index values.
+node_iterator = -1  # A value for iterating node index values.
 
 i = 0
 
-while i < 8:  # Keep looping until lowest gini coefficient reaches a certain value.
+while i < 10:  # Keep looping until lowest gini coefficient reaches a certain value.
 
     feature_values = []
     # The minimum gini coefficient observed in data set during each run of the analysis
@@ -66,31 +66,38 @@ while i < 8:  # Keep looping until lowest gini coefficient reaches a certain val
             if min_count[column_location] > 0:
                 relevant_feature = column_location  # Record feature index value where lowest gini coefficient is located.
 
+    # Remove already used feature values from data set.
     data = extract_used_features(data, min_count_feature_values, relevant_feature)
 
     i += 1
-#
-#         # This breaks the loop if the gini coefficient exceeds the given value in order to avoid overfitting.
-#         if minimum_gini > 0.35:
-#             break
-#
-#         print(minimum_gini)
-#
-    # remaining_features = [z for z in feature_values[relevant_feature] if z not in min_count_feature_values]
-    #
-    # if parent_node == 'null':
-    #     node_name = "Column", relevant_feature
-    #     root = "Column", relevant_feature
-    #     # root = Node(node_name)
-    #     parent_node = min_count_feature_values
-    # else:
-    #     if node_iterator % 2 == 0:
-    #         node["string{0}".format(node_iterator)] = Node(min_count_feature_values, parent=parent_node)
-    #     else:
-    #         node["string{0}".format(node_iterator)] = Node(remaining_features, parent=parent_node)
-    #     node_iterator += 1
-    #
-    # print(RenderTree([root]))
+
+    remaining_features = [z for z in feature_values[relevant_feature] if z not in min_count_feature_values]
+
+    parent_node_name = []
+
+    if node_iterator > -2:
+        if parent_node == 'null':
+            for x in range(0, len(min_count_feature_values)):
+                parent_node_name.append(min_count_feature_values[x])
+                if x < len(min_count_feature_values) - 1:
+                    parent_node_name.append('/')
+            node_name = "Column", str(relevant_feature)
+            node_name = ''.join(node_name)
+            root_name = node_name
+            root = Node(node_name)
+            parent_node_name = ''.join(parent_node_name)
+        else:
+            if node_iterator % 2 == 0:
+                node["string{0}".format(node_iterator)] = Node([min_count_feature_values], parent=[parent_node_name])
+            else:
+                node["string{0}".format(node_iterator)] = Node([remaining_features], parent=[parent_node_name])
+
+        node_iterator += 1
+
+    left_node = feature_values[relevant_feature]
+    right_node = remaining_features
+
+    print(RenderTree(root))
 #
 #     data = pd.DataFrame(data)  # Convert numpy array to pandas Dataframe to more easily remove rows.
 #
